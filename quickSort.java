bd921fab214e6f8b0337fc8c32fd6b539613272e
wenhua-1024 <76110805+wenhua-1024@users.noreply.github.com>
2021-06-29 21:15:56 +0800

    Add files via upload

---
+++ quickSort.java
@@ -0,0 +1,39 @@
+public class quickSort {
+    public static void main(String[] args) {
+        int[] arr = {4,6,8,2,3,5,9,0,1,7};
+        sort(arr, 0, arr.length-1);
+        for(int i=0; i<arr.length; i++)
+            System.out.print(arr[i]);
+    }
+
+    public static void sort(int[] arr, int start, int end){
+        if(start>=end)
+            return;
+        int left = start;
+        int right = end;
+        int tmp = arr[start];
+        int flag = 1;
+        while(left<right){
+            if(flag==0){
+                if(arr[left]>tmp){
+                    arr[right] = arr[left];
+                    flag=1;
+                }
+                else
+                    left++;
+            }
+            else if(flag==1){
+                if(arr[right]<tmp){
+                    arr[left++] = arr[right];
+                    flag=0;
+                }
+                else{
+                    right--;
+                }
+            } 
+        }
+        arr[left]=tmp;
+        sort(arr, start, left-1);
+        sort(arr, left + 1, end);
+    }
+}
